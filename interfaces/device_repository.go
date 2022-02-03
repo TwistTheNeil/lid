2820242cbefafddf11ef8defb1a35f7768a584b7
Neil Castelino <neil.fl93@gmail.com>
2022-02-03 01:26:24 -0500

    chore: fix arg name

--- interfaces/device_repository.go
+++ interfaces/device_repository.go
@@ -5,7 +5,7 @@ import "lid/models"
 type DeviceRepository interface {
 	Create(device_name string, device_uuid string) error
 	FindByName(device_name string) (models.Device, error)
-	FindByUUID(uuid string) (models.Device, error)
+	FindByUUID(device_uuid string) (models.Device, error)
 	FindAll() ([]models.Device, error)
 	DeleteByName(device_name string) error
 	DeleteByUUID(device_uuid string) error
