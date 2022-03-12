b48d615e69f90bac4e58d01174a9dbdfc5486b47
Neil Castelino <neil.fl93@gmail.com>
2022-03-12 11:15:06 -0500

    feat(frontend): add navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <router-view />
+  <Navbar />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
 <script>
@@ -7,9 +10,13 @@ import { onMounted } from "vue";
 
 import { useFileStore } from "@/store/fileStore";
 import { useStorageDeviceStore } from "@/store/storageDeviceStore";
+import Navbar from "@/components/Navbar";
 
 export default {
   name: "Home",
+  components: {
+    Navbar,
+  },
   setup() {
     const fileStore = useFileStore();
     const storageDevicesStore = useStorageDeviceStore();
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- replace with container-fluid if we want left aligned links -->
+    <div class="container">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link
+              class="nav-link"
+              active-class="active"
+              aria-current-value="page"
+              :to="{ name: 'Home' }"
+            >
+              Files
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Devices</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
