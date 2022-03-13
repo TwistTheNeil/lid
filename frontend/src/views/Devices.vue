23a0823fbc72bcf244835f940b81bd20c873af55
Neil Castelino <neil.fl93@gmail.com>
2022-03-13 15:32:15 -0400

    feat(frontend): add some more info about how many items we track

--- frontend/src/views/Devices.vue
+++ frontend/src/views/Devices.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="home">
     <SearchbarInput />
+
+    <span class="mx-2">
+      <small>
+        Tracking {{ storageDevices.length }} storage device{{
+          storageDevices.length === 1 ? "" : "s"
+        }}
+      </small>
+    </span>
+
     <DeviceList />
   </div>
 </template>
 
 <script>
+import { storeToRefs } from "pinia";
+
 import SearchbarInput from "@/components/SearchbarInput.vue";
 import DeviceList from "@/components/DeviceList.vue";
+import { useStorageDeviceStore } from "@/store/storageDeviceStore";
 
 export default {
   name: "Devices",
   components: {
     SearchbarInput,
     DeviceList,
+  },
+  setup() {
+    const storageDeviceStore = useStorageDeviceStore();
+    const { devices: storageDevices } = storeToRefs(storageDeviceStore);
+    return {
+      storageDevices,
+    };
   },
 };
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="home">
     <SearchbarInput />
+
+    <span class="mx-2">
+      <small>
+        Tracking {{ files.length }} file{{ files.length === 1 ? "" : "s" }}
+      </small>
+    </span>
     <FileList />
   </div>
 </template>
 
 <script>
+import { storeToRefs } from "pinia";
+
 import SearchbarInput from "@/components/SearchbarInput.vue";
 import FileList from "@/components/FileList.vue";
+import { useFileStore } from "@/store/fileStore";
 
 export default {
   name: "Home",
   components: {
     SearchbarInput,
     FileList,
+  },
+  setup() {
+    const fileStore = useFileStore();
+    const { files } = storeToRefs(fileStore);
+    return {
+      files,
+    };
   },
 };
 </script>
