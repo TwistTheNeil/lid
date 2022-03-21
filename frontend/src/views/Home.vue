a71f35f66da9adfdee03a470d05a2244f01ab02b
Neil Castelino <neil.fl93@gmail.com>
2022-03-20 22:58:01 -0400

    feat: show total size of files tracked

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -4,19 +4,24 @@
 
     <span class="mx-2">
       <small>
-        Tracking {{ files.length }} file{{ files.length === 1 ? "" : "s" }}
+        Tracking {{ files.length }} file{{ files.length === 1 ? "" : "s" }} ({{
+          totalStorageSpaceUsed
+        }})
       </small>
     </span>
+
     <FileList />
   </div>
 </template>
 
 <script>
 import { storeToRefs } from "pinia";
+import { computed } from "vue";
 
 import SearchbarInput from "@/components/SearchbarInput.vue";
 import FileList from "@/components/FileList.vue";
 import { useFileStore } from "@/store/fileStore";
+import { bytesToHumanReadableUnits } from "../services/dataSizeConversion";
 
 export default {
   name: "Home",
@@ -27,8 +32,15 @@ export default {
   setup() {
     const fileStore = useFileStore();
     const { files } = storeToRefs(fileStore);
+    const totalStorageSpaceUsed = computed(() => {
+      return bytesToHumanReadableUnits(
+        files.value.reduce((a, c) => a + c.size, 0)
+      );
+    });
+
     return {
       files,
+      totalStorageSpaceUsed,
     };
   },
 };
