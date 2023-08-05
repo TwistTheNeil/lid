48fdca1cc1b3e4446724abdbe0040ba8fe2d9ee0
Neil Castelino <neil.fl93@gmail.com>
2023-08-05 01:59:02 -0400

    fix: db_init with --db persistence issue

--- cmd/db_init.go
+++ cmd/db_init.go
@@ -1,16 +1,16 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
 	"lid/models"
 	"lid/services/logger"
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -20,26 +20,27 @@ import (
 var dbInitCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a sqlite3 database",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+		switch debugLevel {
+		case -1, 0, 1, 2, 3, 4, 5, 6, 7:
+			zerolog.SetGlobalLevel(zerolog.Level(debugLevel))
+		default:
+			zerolog.SetGlobalLevel(zerolog.InfoLevel)
+		}
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		log := logger.CreateLogger("db_init.run")
 
-		if len(args) == 0 {
-			log.Error("No database file path argument provided", fmt.Errorf("no database file path provided"))
-			os.Exit(1)
-		}
-
-		if len(args) > 1 {
-			log.Warn("Using only first argument to create database file")
-		}
-
 		// TODO: check if db exists
-		f, err := os.Create(args[0])
+		f, err := os.Create(database)
 		if err != nil {
 			panic(err)
 		}
 		defer f.Close()
 
-		db, err := gorm.Open(sqlite.Open(args[0]), &gorm.Config{})
+		db, err := gorm.Open(sqlite.Open(database), &gorm.Config{})
 		if err != nil {
 			panic(err)
 		}
@@ -52,7 +53,7 @@ var dbInitCmd = &cobra.Command{
 
 		sqlDB.Close()
 
-		log.Info("Database initialized at " + args[0])
+		log.Info("Database initialized at " + database)
 	},
 }
 
