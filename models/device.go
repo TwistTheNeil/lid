0641882c34c632bce144b7b7062ff674beee2eb5
Neil Castelino <neil.fl93@gmail.com>
2023-08-07 19:29:12 -0400

    chore: omit empty file lists for when they are not preloaded

--- models/device.go
+++ models/device.go
@@ -7,5 +7,5 @@ type Device struct {
 	Size      int64     `gorm:"not null" json:"size"`
 	UUID      string    `gorm:"primaryKey" json:"uuid"`
 	CreatedAt time.Time `json:"-"`
-	Files     []*File   `gorm:"many2many:file_devices;" json:"files"`
+	Files     []*File   `gorm:"many2many:file_devices;" json:"files,omitempty"`
 }
