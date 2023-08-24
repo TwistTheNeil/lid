cb23f0e58d4e35cfa0af20b339aa1bcae547bef4
Neil Castelino <neil.fl93@gmail.com>
2023-08-23 22:53:28 -0400

    feat: move to vite

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,11 +2,9 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es2022: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
